a47682c66c3b0011af2bbd1e7a2541d26a95d144
Munaf_Ahmed <munafcoder@gmail.con>
2025-11-20 20:39:50 +0500

    desigining proper std array

--- student_array.cpp
+++ student_array.cpp
@@ -3,17 +3,24 @@
 using namespace std;
 int main()
 {
-    string std_name[3];
+    int num_std, num_courses;
+    cout << "Enter number of students in class : " << endl;
+    cin >> num_std;
+    cout << "Enter number of courses : " << endl;
+    cin >> num_courses;
+    string std_name[num_std], std_courses[num_courses];
+    // int std_score[num_std][num_courses];
 
-    cout << "Enter name of students: \n";
     for (string &name : std_name) // ✔ use string reference
     {
+        cout << "Enter name of student: ";
         cin >> name;
     }
 
-    for (const string &name : std_name) // ✔ print
+    for (int i = 0; i < num_courses; i++) // ✔ print
     {
-        cout << name << " ";
+        cout << "Enter name of " << i + 1 << " course : ";
+        cin >> std_courses[i];
     }
 
     system("pause");
