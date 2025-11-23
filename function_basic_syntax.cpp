168283aaeeacb726162de49610cb3b45354a4a9f
Munaf_Ahmed <munafcoder@gmail.con>
2025-11-23 21:51:22 +0500

    adding sumofdigit function

--- function_basic_syntax.cpp
+++ function_basic_syntax.cpp
@@ -28,15 +28,38 @@ void printarr(int arr[], int size_arr)
     cout << endl;
 }
 
+int sumofdigit(int num)
+{
+    int digitsum = 0;
+    while (num > 0)
+    {
+        int lastdigit = num % 10;
+        num /= 10;
+        digitsum += lastdigit;
+    }
+    return digitsum;
+}
+
 int main(void)
 {
+    // Calling factorial function
     int n;
     cout << "Enter any number to find factorial : " << endl;
     cin >> n;
     cout << "The factorial of " << n << " is " << factorial(n) << endl;
-    cout << "Min value is: " << min(10, 20) << endl;
+
+    // Calling Mymin function
+    cout << "Min value is: " << myMin(10, 20) << endl;
+
+    // Calling printarr function
     int myarr[5] = {10, 40, 20, 30, 9};
     printarr(myarr, 5);
+    cout << "Enter any number : " << endl;
+
+    // calling sum of digit function
+    int number;
+    cin >> number;
+    cout << "Ther sum of your number is " << sumofdigit(number) << endl;
     system("pause");
     return 0;
 }
