c57a388190c9ec6cb4ea901fd7fbe9331cda4561
Munaf_Ahmed <munafcoder@gmail.con>
2025-11-24 14:52:53 +0500

    add binary to decimal converter

--- decimal_binary.cpp
+++ decimal_binary.cpp
@@ -17,8 +17,32 @@ int dec_converter(int n)
     return ans;
 }
 
+int bintodec(int num)
+{
+    int pow = 1, ans = 0;
+
+    while (num > 0)
+    {
+        int rem = num % 10;
+        ans += rem * pow;
+        pow *= 2;
+        num /= 10;
+    }
+    return ans;
+}
+
 int main()
 {
-    cout << dec_converter(42);
+    cout << "The binary number of 42 is : " << dec_converter(42) << endl;
+    cout << "The binary number from 1 to 10 : " << endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << dec_converter(i) << endl;
+    }
+    cout << "Write any Binary number to convert it into decimal number : " << endl;
+    int n;
+    cin >> n;
+    cout << bintodec(n) << endl;
+    system("pause");
     return 0;
 }
