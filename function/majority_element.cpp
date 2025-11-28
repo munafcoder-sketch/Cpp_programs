2300d8ccd80fc1bacd618504997a62b16c9eeb6a
Munaf_Ahmed <munafcoder@gmail.con>
2025-11-28 20:51:37 +0500

    add more optimise method

--- function/majority_element.cpp
+++ function/majority_element.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
+
+//  print array
+void printarr(vector<int> &arr)
+{
+    // int size = arr.size();
+    cout << "[ ";
+    for (int element : arr)
+    {
+        cout << element << " ";
+    }
+    cout << "]" << endl;
+}
 
 // brute method
 int majele(vector<int> &arr)
@@ -21,10 +34,40 @@ int majele(vector<int> &arr)
     }
     return -1;
 }
+
+// more optimise approach
+int majorityelement(vector<int> &arr)
+{
+    sort(arr.begin(), arr.end());
+    int fre = 1, n = arr.size(), ans = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] == arr[i - 1])
+        {
+            fre++;
+        }
+        else
+        {
+            fre = 1;
+            ans = arr[i];
+        }
+        if (fre < n / 2)
+        {
+            return ans;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
+    // calling functoin using brute method
     vector<int> myarr = {1, 1, 2, 1, 2, 1};
+    printarr(myarr);
     cout << "The majority elements present in arr is : " << majele(myarr) << endl;
+
+    // NOw using more optimise method
+    cout << "The majority elemnts present in array is : " << majorityelement(myarr) << endl;
 
     system("pause");
     return 0;
