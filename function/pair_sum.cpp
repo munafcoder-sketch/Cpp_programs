84abb584a6a1af178aea59afd020e089471d87a5
Munaf_Ahmed <munafcoder@gmail.con>
2025-11-28 19:18:10 +0500

    adding optimise way of pair sum

--- function/pair_sum.cpp
+++ function/pair_sum.cpp
@@ -24,11 +24,40 @@ vector<int> pairsum(vector<int> arr, int target)
     return ans;
 }
 
+// usig optamastic method
+vector<int> PairSum(vector<int> arr, int target)
+{
+    int n = arr.size();
+    vector<int> ans;
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
+    {
+        int Mysum = arr[i] + arr[j];
+        if (target > Mysum)
+        {
+            i++;
+        }
+        else if (target < Mysum)
+        {
+            j--;
+        }
+        else
+        {
+            ans.push_back(arr[i]);
+            ans.push_back(arr[j]);
+            return ans;
+        }
+    }
+    cout << "Your targated number doesn't exsit";
+    return ans;
+}
+
 int main()
 {
     vector<int> myarr = {2, 6, 7, 15, 17};
     int target = 13;
-    vector<int> ans = pairsum(myarr, target);
+    vector<int> ans = PairSum(myarr, target);
     cout << ans[0] << " " << ans[1] << endl;
     system("pause");
     return 0;
