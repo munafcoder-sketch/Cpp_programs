12300a8d0cffaa8bbbbe387cf415d0325cf5019d
Munaf_Ahmed <munafcoder@gmail.con>
2025-12-17 23:03:41 +0500

    learning about array of pointers in C++.

--- function/pointer.cpp
+++ function/pointer.cpp
@@ -17,6 +17,17 @@ int main()
     increment(a);
     cout << a << endl;
 
+    // array and pointer
+    int arr[] = {1, 5, 3, 6, 7, 8, 12};
+    // assigning array adderess to pointer varible
+    int *ptr1 = arr;
+    // it prints address of array of first index
+    cout << ptr1 << endl;
+    // we can acess array index through pointer like this
+    cout << ptr1[0] << endl;
+    // it prints the address of 3rd index of array
+    cout << ptr1 + 2 << endl;
+
     system("pause");
     return 0;
 }
