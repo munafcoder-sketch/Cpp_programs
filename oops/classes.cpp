cef92be0bcda865a8e78e9decd93a9829ca3ece1
Munaf_Ahmed <munafcoder@gmail.con>
2026-01-18 04:39:25 +0500

    learning constructor

--- oops/classes.cpp
+++ oops/classes.cpp
@@ -48,6 +48,16 @@ public:
         this->age = age;
         // introduction(); wesa we can call the fuction in it but for your dumb brain I don't do this
     }
+
+    // right way to create copy construtor
+    employee(employee &orgobj)
+    {
+        cout << "I am the custom copy constructor : " << endl;
+        this->name = orgobj.name;
+        this->degree = orgobj.degree;
+        this->age = orgobj.age;
+    }
+
     void setsalary(int s)
     {
         salary = s;
@@ -61,6 +71,41 @@ public:
         cout << "Name : " << name << endl;
         cout << "Degree : " << degree << endl;
         cout << "age : " << age << endl;
+        cout << "salary : " << getsalary() << endl;
+    }
+};
+
+// shallow copy
+class students
+{
+public:
+    string name;
+    double *cgpaptr;
+    students(string name, double cgpa)
+    {
+        this->name = name;
+        this->cgpaptr = new double;
+        *cgpaptr = cgpa;
+    }
+    // // this is for shallow constructor
+    // students(students &opj)
+    // {
+    //     this->name = opj.name;
+    //     this->cgpaptr = opj.cgpaptr;
+    // }
+
+    //  to deep constructor we used
+    students(students &obj)
+    {
+        this->name = obj.name;
+        cgpaptr = new double;
+        *cgpaptr = *obj.cgpaptr;
+    }
+
+    void getinfo()
+    {
+        cout << "name : " << name << endl;
+        cout << "cgpa :" << *cgpaptr << endl;
     }
 };
 
@@ -79,7 +124,25 @@ int main()
     employee e1("Munaf", "CS", 19);
     e1.setsalary(90000);
     e1.introduction();
-    cout << e1.getsalary() << endl;
+    // to copy the same data we can do this like this
+    employee e2(e1);
+    e2.introduction();
+
+    // shallow copy constructor
+    // In shallow constructor there is a problem if we change the value of second or other data it will changes others first to
+    // students s1("Munaf ahmed", 8);
+    // s1.getinfo();
+    // students s2(s1);
+    // *s2.cgpaptr = 5.2;
+    // s1.getinfo();
+
+    // here is deep construtor example the above code is same but the main changes is in class instrutor
+    students s1("Munaf ahmed", 8);
+    s1.getinfo();
+    students s2(s1);
+    *s2.cgpaptr = 5.2; // here first data is save
+    s1.getinfo();
+
     system("pause");
     return 0;
 }
